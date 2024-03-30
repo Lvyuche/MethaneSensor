3d9437bd8b8402e9d769e05a64d8cec6e69d21d2
Hao-yu Xie <112199186+HaoyuXie98@users.noreply.github.com>
2024-03-29 18:21:28 -0700

    Update mapping.py

--- src/mapping.py
+++ src/mapping.py
@@ -15,7 +15,7 @@ y = data['Nano'].values
 
 # Split the data into training and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
+ 
 # Create polynomial features
 degree = 2
 poly_features = PolynomialFeatures(degree=degree)
