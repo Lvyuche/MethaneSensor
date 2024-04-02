5d21ec49445af76fa843154f18d574ad754a8337
Hao-yu Xie <112199186+HaoyuXie98@users.noreply.github.com>
2024-04-02 00:58:50 -0700

    4V mapping finished

--- src/mapping.py
+++ src/mapping.py
@@ -6,31 +6,37 @@ from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 
 # Load the CSV file
-file_path = 'src/5500_sensor_data_3.29.csv'
+file_path = 'sensor/5500_sensor_data_4.0.csv'
 data = pd.read_csv(file_path)
 
-# Filter out rows where '|Diff|' is greater than 500
+# # Filter out rows where '|Diff|' is greater than 500
+# print(len(data))
+# data = data[data['|Diff|'] <= 3000]
+# print(len(data))
+
+# Filter out rows where MQ-4 > 10000
 print(len(data))
-data = data[data['|Diff|'] <= 1500]
+data = data[data['Nano'] <= 5000]
 print(len(data))
 
-# Filter out rows where 'Nano' column is 0
+# Filter out rows where 'Nano' column is 0 and diff >= 500
 print(len(data))
-data = data[data['Nano'] != 0]
+# data = data[(data['Nano'] != 0) | (data['|Diff|'] <= 100)]
+data = data[(data['MQ-4'] != 0)]
+data = data[(data['Nano'] != 0)]
 print(len(data))
 
 # Prepare the data
-X = data['MQ-4'].values.reshape(-1, 1)
+X = data['AO'].values.reshape(-1, 1)
 y = data['Nano'].values
 
 # Split the data into training and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
- 
+
 # Create polynomial features
 degree = 2
 poly_features = PolynomialFeatures(degree=degree)
 X_poly_train = poly_features.fit_transform(X_train)
-
 
 # Train the model
 model = LinearRegression()
--- src/serial_measure.py
+++ src/serial_measure.py
@@ -1,15 +1,58 @@
 import serial
 import re
 
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+
+# Load the CSV file and preprocess
+file_path = 'sensor/5500_sensor_data_4.0.csv'
+data = pd.read_csv(file_path)
+
+# data = data[data['|Diff|'] <= 3000]
+data = data[data['Nano'] <= 10000]
+# data = data[(data['Nano'] != 0) | (data['|Diff|'] <= 500)]
+data = data[(data['MQ-4'] != 0)]
+data = data[(data['Nano'] != 0)]
+
+# Prepare the data
+X = data['AO'].values.reshape(-1, 1)
+y = data['Nano'].values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Create polynomial features
+degree = 2
+poly_features = PolynomialFeatures(degree=degree)
+X_poly_train = poly_features.fit_transform(X_train)
+
+# Train the model
+model = LinearRegression()
+model.fit(X_poly_train, y_train)
+
+# Predict and plot
+X_poly = poly_features.transform(X)
+y_pred = model.predict(X_poly)
+
+def mapping (mq4_reading):
+    c, b, a = model.coef_
+    intercept = model.intercept_
+    nano_reading = a * mq4_reading * mq4_reading + b * mq4_reading + intercept
+
+    return nano_reading
+
 # Serial port configuration
 serial_port = '/dev/tty.usbmodem1202'  # Change to your serial port
 baud_rate = 9600
+R0 = 5500
 
 # Directory where the CSV file will be saved
-csv_file_directory = './IoT DR/sensor data/'
+csv_file_directory = 'src/'
 
 # Adjusted regular expression pattern to match and capture three floating-point numbers
-data_pattern = re.compile(r'^(-?\d+\.\d+),(-?\d+\.\d+),(-?\d+\.\d+)$')
+data_pattern = re.compile(r'^(-?\d+\.\d+),(-?\d+\.\d+)$')
 
 def save_sensor_data():
     with serial.Serial(serial_port, baud_rate, timeout=1) as ser:
@@ -23,16 +66,24 @@ def save_sensor_data():
                 if match:  # Check if the line matches the pattern
                     if not file_opened:
                         first_number = match.group(1)  # Extract the first number for file name
-                        csv_file_path = f'{csv_file_directory}{first_number}_sensor_data.csv'
-                        file = open(csv_file_path, 'w')  # Open the CSV file for writing
+                        csv_file_path = f'{csv_file_directory}{R0}_sensor_data_4.0_ao.csv'
+                        file = open(csv_file_path, 'a')  # Open the CSV file for writing
                         print(f"Saving sensor data to {csv_file_path}...")
                         # Adjusted CSV header with new columns
                         file.write("AO,MQ-4,Nano,Diff,|Diff|\n")
                         file_opened = True
                     else:
+
                         # Extract and process the numbers
-                        ao, mq4, nano = match.groups()
-                        mq4_int = int(float(mq4))  # Convert to integer (keep integer part)
+                        ao, nano = match.groups()
+                        ao = float(ao)
+                        # sensor_volt = ao * (3.54)
+                        # RS_gas = ((3.54 - sensor_volt) - 1.0) * 1000 / sensor_volt
+                        # ratio = RS_gas / R0
+                        # ppm = 1000*pow((ratio),-2.95)
+
+                        ppm = mapping(ao)
+                        mq4_int = int(float(ppm))  # Convert to integer (keep integer part)
                         nano_int = int(float(nano))  # Convert to integer (keep integer part)
                         diff = mq4_int - nano_int
                         abs_diff = abs(diff)
